daa3ee7bb3800b0cdfc889bdfacd6b5c6b20e574
kurupical <kurupical@gmail.com>
2017-09-05 17:33:54 +0900

    #5

--- main.java
+++ main.java
@@ -3,6 +3,6 @@ import hashmap_test.HashMap_Original;
 public class main {
     public static void main(String[] args) {
         HashMap_Original<String, String> test = new HashMap_Original();
-
+        test.put("a","b");
     }
 }
